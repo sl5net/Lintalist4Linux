4bbb79f62fab08275f771c2a15347a2a07850e17
sl5net <sl5softwarelab@gmail.com>
2020-11-06 16:30:57 +0100

    .

--- install.sh
+++ install.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 # Start it for e.g. so:
+#/‾‾‾ problems installing autohokey using this line
+# clear ; cd /tmp ; wget https://raw.githubusercontent.com/sl5net/Lintalist4Linux/master/install.sh ./install.sh ;    sudo chmod +wx ./install.sh ; ./install.sh ; rm ./install.sh ;
+#/‾‾‾ no problems # chmod +rwx
 # clear ; cd /tmp ; wget https://raw.githubusercontent.com/sl5net/Lintalist4Linux/master/install.sh ./install.sh ;    sudo chmod 777 ./install.sh ; ./install.sh ; rm ./install.sh ;
+#/‾‾‾ no problems bash: ./install.sh: Permission denied
 # clear ; sudo wget https://raw.githubusercontent.com/sl5net/Lintalist4Linux/master/install.sh ~/Downloads/install.sh ; cd ~/Downloads ; sudo chmod 750 ./install.sh ; ./install.sh
+#/‾‾‾ solution without sh-script
 # sudo apt -y install autokey-qt ; echo "########## \n Please Exit AutoKey (right Click Taskbar on little Icon) then the installation will continue automatically. \n#########" ; autokey-qt & sleep 3s ; killall -9 autokey-qt ; sudo apt -y install git ; mkdir ~/ahk ; mkdir ~/ahk/github; cd ~/ahk/github/ ; git clone https://github.com/lintalist/lintalist ; cd ~/Downloads/ ; git clone https://github.com/sl5net/Lintalist4Linux ; cp ~/Downloads/Lintalist4Linux/run-run-lintalistAHK-all.py ; cp ~/Downloads/Lintalist4Linux/run-lintalistAHK.ahk ; ~/.config/autokey/data/Sample\ Scripts/ ; sudo dpkg --add-architecture i386 ; wget -nc https://dl.winehq.org/wine-builds/winehq.key; sudo apt-key add winehq.key ; sudo apt-add-repository -y 'deb https://dl.winehq.org/wine-builds/ubuntu/ eoan main' ; sudo add-apt-repository -y ppa:cybermax-dexter/sdl2-backport ; sudo apt update && sudo apt -y install --install-recommends winehq-stable ; wget https://www.autohotkey.com/download/ahk-install.exe ~/Downloads/ ; wine ~/Downloads/AutoHotkey_1.1.33.02_setup.exe ; echo "Please Configure AutohotKey and AutoKey" ; autokey-qt;
+
 sudo apt -y install autokey-qt ; autokey-qt & sleep 3s ; killall -9 autokey-qt
 echo "########## Please Exit AutoKey (right Click Taskbar on little Icon) then the installation will continue automatically. #########"
 sudo apt -y install git || exit
