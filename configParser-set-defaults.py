08f3ba4f6112305e0111e5fdb9458388b47daf0a
sl5net <sl5softwarelab@gmail.com>
2020-11-07 16:21:25 +0100

    add configParser-set-defaults.py

---
+++ configParser-set-defaults.py
@@ -0,0 +1,17 @@
+# whereis here: ~/Documents/github/PycharmProjects/Lintalist4Linux/configParser-set-defaults.py
+
+from pathlib import Path
+import json, os
+home = str(Path.home())
+path = home + "/.config/autokey/data/Sample Scripts/"
+jsonFilePath = home + "/.config/autokey/data/Sample Scripts/.run-run-lintalistAHK-all.json"
+file = open(jsonFilePath)
+data = json.load(file)
+for x in data['hotkey']:
+    data['hotkey'][x] = '<f11>'
+print(data)
+with open(jsonFilePath, 'w') as file:
+    json.dump(data, file, indent=2)
+
+myCmd = 'wine ' + home + '/.wine/drive_c/Program\ Files/AutoHotkey/AutoHotkey.exe "' + home + '/.config/autokey/data/Sample Scripts/configParser-set-ini-defaults.ahk"'
+os.system(myCmd)
